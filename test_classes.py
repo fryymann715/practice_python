59c3f19d33c7844c0e0c3393540fa45a0ab4e362
fryymann715 <ideans715@gmail.com>
2015-06-30 23:37:29 -0700

    modified test classes

--- test_classes.py
+++ test_classes.py
@@ -3,20 +3,22 @@ __author__ = 'Ian'
 import webbrowser
 import time
 
+
 def to_seconds(hours):
     hours = int(hours) * 60 * 60
     return hours
 
 
-def take_break(seconds):
+def take_break(minutes):
+    seconds = int(minutes) * 60
     counter = 0
-    while counter < 3:
+    while counter < 2:
         time.sleep(seconds)
         print("testing!")
-       #webbrowser.open("https://mail.google.com/mail/u/0/?tab=wm#inbox/14e38528a5dbdd12")
+        webbrowser.open("https://mail.google.com/mail/u/0/?tab=wm#inbox/14e38528a5dbdd12")
         counter += 1
 
 
 
 
-take_break()
+take_break(1)

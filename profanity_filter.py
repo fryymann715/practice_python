f628e964ba8db0464d804940992ad97464765bdc
fryymann715 <ideans715@gmail.com>
2015-07-18 11:04:22 -0700

    Fixed Profanity Filer
    Changed the path of the file to include the file name and also moved the
    folder to the C drive instead of within my Documents folders.

--- profanity_filter.py
+++ profanity_filter.py
@@ -1,9 +1,23 @@
+import urllib
+
 __author__ = 'ideans'
 
 def read_text():
-    quotes = open("C:\Users\ideans\Documents\Python_Playground\movie_quotes")
+    quotes = open("C:\python_playground\movie_quotes\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("*****Der be bad words in dat der file!*****")
+    elif "false" in output:
+        print("This file is clean of dem curse words.")
+    else:
+        print("*****Daaaah... It didn't work George.*****")
 
 read_text()
